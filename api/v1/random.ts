8582f2f3b492f385613744013bd9509a4499e3d7
blacktunes <blacktunes@foxmail.com>
2021-12-20 20:21:21 +0800

    feat: 优化随机Hii图API

--- api/v1/random.ts
+++ api/v1/random.ts
@@ -5,24 +5,29 @@ const path = require('path')
 const list = fs.readdirSync(path.join(__dirname, '../../public/memes'))
 
 export default (request: VercelRequest, response: VercelResponse) => {
+  const { redirect } = request.query
   const random = (list[Math.floor(Math.random() * list.length)]).replace(/%/g, '%25')
-  response.setHeader('Content-Type', 'text/html')
-  response
-    .status(200)
-    .send(`
-    <!DOCTYPE html>
-    <html lang="en" style="height: 100%;">
-
-    <head>
-      <meta charset="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>随机Hii图</title>
-    </head>
-    
-    <body style="margin: 0px;background: #0e0e0e;height: 100%;display: flex;align-items: center;justify-content: center;">
-      <img style="max-width: 100%;max-height: 100%;" src="https://cdn.jsdelivr.net/gh/blacktunes/hiiro-memes@master/public/memes/${random}" alt="">
-    </body>
-    
-    </html>
-`)
+  if (redirect !== undefined) {
+    response.redirect(`https://cdn.jsdelivr.net/gh/blacktunes/hiiro-memes@master/public/memes/${random}`)
+  } else {
+    response.setHeader('Content-Type', 'text/html')
+    response
+      .status(200)
+      .send(`
+        <!DOCTYPE html>
+        <html lang="en" style="height: 100%;">
+  
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>随机Hii图</title>
+        </head>
+        
+        <body style="margin: 0px;background: #0e0e0e;height: 100%;display: flex;align-items: center;justify-content: center;">
+          <img style="max-width: 100%;max-height: 100%;" src="https://cdn.jsdelivr.net/gh/blacktunes/hiiro-memes@master/public/memes/${random}" alt="">
+        </body>
+        
+        </html>
+      `)
+  }
 }
