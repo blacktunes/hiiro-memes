c6f6faa41e1fb90924f6b301cbedff5991992f04
blacktunes <blacktunes@foxmail.com>
2022-01-15 18:01:50 +0800

    fix: 修复错误

--- api/v1/random.ts
+++ api/v1/random.ts
@@ -1,8 +1,8 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import fs from 'fs-extra'
-import path from 'path'
+const fs = require('fs')
+const path = require('path')
 
-const list = fs.readJsonSync(path.join(__dirname, '../../dist/list.json'))
+const list = fs.readdirSync(path.join(__dirname, '../../public/memes'))
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const random = (list[Math.floor(Math.random() * list.length)]).replace(/%/g, '%25')
