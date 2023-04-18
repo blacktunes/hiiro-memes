c6b7edebbc6e41a9060f7092aa62e1fbdd55d8fd
blacktunes <blacktunes@foxmail.com>
2023-04-18 16:58:58 +0800

    feat: 更换图床

--- src/components/Memes.vue
+++ src/components/Memes.vue
@@ -2,55 +2,42 @@
   <div ref="wrapper">
     <div class="memes-list" ref="list">
       <a
-        v-for="item in memes"
-        :key="item"
+        v-for="(item, index) in memes"
+        :key="index"
         class="memes"
-        :href="URL + decode(item)"
+        :href="item"
         target="__black"
       >
-        <img src="@/assets/image/loading.png"
-        v-lazy="URL + decode(item)" @load="onLoad" @error.once="onError"/>
+        <img
+          src="@/assets/image/loading.gif"
+          v-lazy="item"
+          @error.once="onError"
+        />
       </a>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import Setting from '@/../setting/setting.json'
+import errorImG from '@/assets/image/error.jpg'
+import memes from '@/../public/memes.json'
 import { ref, onMounted } from 'vue'
 
-const CDN = Setting['CDN']
-const URL = process.env.NODE_ENV === 'production' && CDN ? `${CDN}/` : '/memes/'
+memes.sort(() => Math.random() - 0.5)
 
-const wrapper = ref()
-const list = ref()
+const wrapper = ref<HTMLElement>()
+const list = ref<HTMLElement>()
 
-const decode = (str: string) => {
-  return str.replace(/%/g, '%25')
-}
-
-const memesList = require.context('@/../public/memes', false, /.*$/).keys().map(item => {
-  return item.substring(2)
-})
-
-const memes = ref(memesList)
-
-const onLoad = (el) => {
-  if (!el.path[0]) return
-  if (el.path[0].offsetHeight > 160) {
-    el.path[0].style.width = 'auto'
-    el.path[0].style.height = '100%'
-  }
-}
-
-const onError = (event) => {
-  if (event?.path?.[0]) {
-    event.path[0].src = require('@/assets/image/loading.png').default
+const onError = (e: any) => {
+  if (e?.path?.[0]) {
+    e.path[0].src = errorImG
   }
 }
 
 const setWarpperWidth = () => {
-  list.value.style.width = (((wrapper.value.offsetWidth / 160) | 0) || 1) * 160 + 'px'
+  if (list.value && wrapper.value) {
+    list.value.style.width = (((wrapper.value.offsetWidth / 160) | 0) || 1) * 160 + 'px'
+  }
 }
 
 onMounted(() => {
@@ -68,14 +55,16 @@ onMounted(() => {
   margin auto
 
   .memes
+    overflow hidden
     display flex
     align-items center
     justify-content center
     height 160px
     width 160px
     box-sizing border-box
-    border 1px solid rgba(0,0,0,0.05)
+    border 1px solid rgba(0, 0, 0, 0.05)
 
     img
-      width 100%
+      max-width 100%
+      max-height 100%
 </style>
