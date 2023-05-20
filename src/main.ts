42432a58a2c98d2d5e6e5aa4fa97fff902525c22
blacktunes <blacktunes@foxmail.com>
2023-05-20 16:57:05 +0800

    feat: 增加访问统计

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import lazyload from '@/assets/scripts/lazyload'
+import '@/assets/style/animation.styl'
+import '@/assets/style/transition.styl'
+import router from '@/router'
+import { inject } from '@vercel/analytics'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from '@/router'
-import lazyload from '@/assets/scripts/lazyload'
-import '@/assets/style/transition.styl'
-import '@/assets/style/animation.styl'
 
-createApp(App).use(router).use(lazyload).mount('#app')
+createApp(App)
+  .use(router)
+  .use(lazyload)
+  .mount('#app')
+
+if (process.env.NODE_ENV !== 'development') {
+  inject()
+}
