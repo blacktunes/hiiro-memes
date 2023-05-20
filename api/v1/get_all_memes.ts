38ebb0b80c0970d4fd99cdd4f4cdd516108fc685
blacktunes <blacktunes@foxmail.com>
2023-05-20 16:54:00 +0800

    fix: 修复API异常

--- api/v1/get_all_memes.ts
+++ api/v1/get_all_memes.ts
@@ -1,6 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import fs from 'fs'
-import path from 'path'
+
+const fs = require('fs')
+const path = require('path')
 
 const list = JSON.parse(fs.readFileSync(path.join(__dirname, '../../public/memes.json'), 'utf-8'))
 
